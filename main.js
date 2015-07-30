986038a4a2b17d6b8b328433fac82154ab3b56e5
Kevin "Schmidty" Smith <shenanigans@kaztl.com>
2015-07-29 19:02:37 -0700

    LimitDropCache initial

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 module.exports = {
     ChainCache:         require ('./lib/ChainCache'),
+    LimitDropCache:     require ('./lib/LimitDropCache'),
     DocumentChainCache: require ('./lib/DocumentChainCache'),
     RangeIndex:         require ('./lib/RangeIndex')
 };
